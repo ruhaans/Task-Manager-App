9f2506afd8afb3d966103c817290af0a51223ff2
Ruhaan Sikilgar <rruhaan506@gmail.com>
2025-05-30 00:49:38 +0530

    Rendered SQL

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,10 @@ app.config.from_object(Config)
 db= SQLAlchemy(app)
 jwt= JWTManager(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 from routes import *
 
 #if __name__ == '__main__':
--- backend/config.py
+++ backend/config.py
@@ -9,4 +9,4 @@ class Config:
 
     JWT_SECRET_KEY= os.getenv('JWT_SECRET_KEY', 'a3f5e7c2b9d1e8f4a0c3d6e7b8a9f0c1e2d3f4a5b6c7d8e9f0a1b2c3d4e5f6a7')
 
-    DEBUG= True
+    DEBUG= False
